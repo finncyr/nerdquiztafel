3534772c0169f41350dfaff1bca88651c53b9443
svolume <pilot495gmail.com>
2020-09-10 20:53:46 +0200

    fixed rounds Now you cant go to round < 1 and the description on the doubler button is case sensitive.

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,4 @@
+let round = 1;
 $(document).ready(function () {
 
     $('td').click(function () {
@@ -18,44 +19,21 @@ $(document).ready(function () {
             var a = parseInt($(this).html());
             $(this).html(a * 2);
         });
+        round = round + 1;
+        const roundtemp = round + 1;
+        document.getElementById("doubler").innerHTML = "Runde " + roundtemp;
     });
     $("#divider").click(function () {
-        $("#myTable").find('td').each(function () {
-            var b = parseInt($(this).html());
-            $(this).html(b / 2);
-        });
+        if (round > 1) {
+            $("#myTable").find('td').each(function () {
+                var a = parseInt($(this).html());
+                $(this).html(a / 2);
+            });
+            const roundtemp = round;
+            round = round - 1;
+            document.getElementById("doubler").innerHTML = "Runde " + roundtemp;
+        }
+
     });
 
-    
-
-    // Hintergrundfarbe ändert sich bei bestimmten Keypress (Pfeiltasten links, oben, rechts)
-    body.onkeydown = checkKey;
-
-    function checkKey(e) {
-    e = e || window.event;
-        
-    // linke Pfeiltaste
-    if (e.keyCode == 37) {
-        document.getElementById('body').className = "show";
-        var myVar =  setInterval(function(){  document.getElementById('body').className = ""; clearInterval(myVar);}, 500);
-        setTimeout(function(){ alert("Orange hat gedrückt!"); }, 500);
-              
-    }
-    // oben Pfeiltaste
-    else if (e.keyCode == 38) {
-        document.getElementById('body').className = "show2";
-        var myVar =  setInterval(function(){  document.getElementById('body').className = ""; clearInterval(myVar);}, 500);
-        setTimeout(function(){ alert("Blau hat gedrückt!"); }, 500);        
-
-    }
-    // rechts Pfeiltaste
-    else if (e.keyCode == 39) {
-    document.getElementById('body').className = "show3";
-    var myVar =  setInterval(function(){  document.getElementById('body').className = ""; clearInterval(myVar);}, 500);
-    setTimeout(function(){ alert("Grün hat gedrückt!"); }, 500);        
-    }
-}
-    
-
 });
-
