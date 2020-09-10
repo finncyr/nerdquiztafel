30c99690cc1b87a7ac0aa726953d0ca15cdc81ab
marcb4 <manni.panni12345@gmail.com>
2020-09-10 17:00:28 +0200

    BG now blinking when arrow keys are pressed
    use left, up, right for different colors

--- src/js/index.js
+++ src/js/index.js
@@ -21,9 +21,41 @@ $(document).ready(function () {
     });
     $("#divider").click(function () {
         $("#myTable").find('td').each(function () {
-            var a = parseInt($(this).html());
-            $(this).html(a / 2);
+            var b = parseInt($(this).html());
+            $(this).html(b / 2);
         });
     });
 
+    
+
+    // Hintergrundfarbe ändert sich bei bestimmten Keypress (Pfeiltasten links, oben, rechts)
+    body.onkeydown = checkKey;
+
+    function checkKey(e) {
+    e = e || window.event;
+        
+    // linke Pfeiltaste
+    if (e.keyCode == 37) {
+        document.getElementById('body').className = "show";
+        var myVar =  setInterval(function(){  document.getElementById('body').className = ""; clearInterval(myVar);}, 500);
+        setTimeout(function(){ alert("Orange hat gedrückt!"); }, 500);
+              
+    }
+    // oben Pfeiltaste
+    else if (e.keyCode == 38) {
+        document.getElementById('body').className = "show2";
+        var myVar =  setInterval(function(){  document.getElementById('body').className = ""; clearInterval(myVar);}, 500);
+        setTimeout(function(){ alert("Blau hat gedrückt!"); }, 500);        
+
+    }
+    // rechts Pfeiltaste
+    else if (e.keyCode == 39) {
+    document.getElementById('body').className = "show3";
+    var myVar =  setInterval(function(){  document.getElementById('body').className = ""; clearInterval(myVar);}, 500);
+    setTimeout(function(){ alert("Grün hat gedrückt!"); }, 500);        
+    }
+}
+    
+
 });
+
