7af273a59b255c71b8aa6b6625d3f7c6505a6cc0
marcb4 <manni.panni12345@gmail.com>
2020-09-10 22:38:28 +0200

    updated keypress function again

--- src/js/index.js
+++ src/js/index.js
@@ -14,6 +14,35 @@ $(document).ready(function () {
 
     });
 
+    // Hintergrundfarbe ändert sich bei bestimmten Keypress (Pfeiltasten links, oben, rechts)
+    body.onkeydown = checkKey;
+
+    function checkKey(e) {
+    e = e || window.event;
+
+    // linke Pfeiltaste
+    if (e.keyCode == 37) {
+        document.getElementById('body').className = "show";
+        var myVar =  setInterval(function(){  document.getElementById('body').className = ""; clearInterval(myVar);}, 500);
+        setTimeout(function(){ alert("Orange hat gedrückt!"); }, 500);
+
+    }
+    // oben Pfeiltaste
+    else if (e.keyCode == 38) {
+        document.getElementById('body').className = "show2";
+        var myVar =  setInterval(function(){  document.getElementById('body').className = ""; clearInterval(myVar);}, 500);
+        setTimeout(function(){ alert("Blau hat gedrückt!"); }, 500);        
+
+    }
+    // rechts Pfeiltaste
+    else if (e.keyCode == 39) {
+    document.getElementById('body').className = "show3";
+    var myVar =  setInterval(function(){  document.getElementById('body').className = ""; clearInterval(myVar);}, 500);
+    setTimeout(function(){ alert("Grün hat gedrückt!"); }, 500);        
+    }
+}
+
+
     $("#doubler").click(function () {
         $("#myTable").find('td').each(function () {
             var a = parseInt($(this).html());
@@ -36,4 +65,8 @@ $(document).ready(function () {
 
     });
 
+    
+
 });
+
+
